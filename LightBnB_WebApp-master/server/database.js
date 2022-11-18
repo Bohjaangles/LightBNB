cc118e7a357d110a422af70c728f77686dd69542
Russel Mueller <bohjaangles@gmail.com>
2022-11-18 03:34:13 +0000

    refactoring getallproperties function to be searchable

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -107,10 +107,71 @@ exports.getAllReservations = getAllReservations;
  */
  const getAllProperties = (options, limit = 10) => {
 
+  let queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+    FROM properties JOIN property_reviews ON property_id = properties.id
+  `
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE properties.city LIKE $${queryParams.length}
+    `;
+    if (options.owner_id || options.minimum_price_per_night || options.maximum_price_per_night){
+      queryString += ` AND `;
+    }
+  }
+  if (options.owner_id){
+    queryParams.push(`${options.owner_id}`);
+    if (!options.city){
+      queryString += `WHERE `;
+    }
+    queryString += `properties.owner_id = $${queryParams.length}
+    `;
+    if (options.minimum_price_per_night || options.maximum_price_per_night){
+      queryString += ` AND `;
+    }
+  }
+  if (options.minimum_price_per_night){
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    if (!options.city && !options.owner_id){
+      queryString += `WHERE `;
+    }
+    queryString += `properties.cost_per_night > $${queryParams.length}
+    `;
+    if (options.maximum_price_per_night){
+      queryString += ` AND `;
+    }
+  }
+  if (options.maximum_price_per_night){
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    if (!options.city && !options.owner_id && !options.minimum_price_per_night){
+      queryString += `WHERE `;
+    }
+    queryString += `properties.cost_per_night < $${queryParams.length}
+    `; 
+  }
+
+  queryString += `GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING AVG(rating) >= $${queryParams.length}
+    `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}`;
+
+  console.log('QS', queryString);
   return pool
-    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
-      console.log('GAprops :::', result.rows);
+      console.log('GAprops :::', result);
       return result.rows;
     })
     .catch((err) => {
